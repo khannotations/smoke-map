bb7b427cbc7f5502916f54e6b63037cf8abb96b2
Rafi Khan <rafi.fa.khan@gmail.com>
2014-12-22 17:04:01 -0600

    added sass, removed unused counties, added states, county-coloring etc

--- scripts/script.js
+++ scripts/script.js
@@ -1,108 +1,183 @@
 "use strict";
 
 $(document).ready(function() {
-  var FUTURE_SW, PRESENT_SW, po, map;
-
+  var ALL_SW, po, map;
+  var KEYS = {
+    future: {
+      length: "Future_length",
+      intensity: "Future_intensity",
+      seasonLenght: "Future_seasonlength",
+      sw6: "Future_SWnum6y",
+      sw1: "Future_SWnum1y",
+      swDay: "Future_SWdaynum",
+      index: "Future_index2"
+    },
+    present: {
+      length: "PD_length",
+      intensity: "PD_intensity",
+      seasonLenght: "PD_seasonlength",
+      sw6: "PD_SWnum6y",
+      sw1: "PD_SWnum1y",
+      swDay: "PD_SWdaynum",
+      index: "PD_index2"
+    }
+  };
+  var MODE = "present"; 
+  var DATA_CONTAINER_ID = "#smoke-map-data-container";
   // Load map
   po = org.polymaps;
   map = po.map()
-    .container(document.getElementById("map").appendChild(po.svg("svg")))
+    .container(document.getElementById("smoke-map-map").appendChild(po.svg("svg")))
     // .add(po.interact())
-    .zoom(5.1)
-    .center({lat: 41, lon: -105});
-
+    .zoom(4.9)
+    .center({lat: 41, lon: -120});
+  // Load state shapes
   map.add(po.geoJson()
-    .url("./data/counties-sm.json")
-    // .features()
+    .url("./data/states.json")
+    .id("states"));
+  // Load county shapes
+  map.add(po.geoJson()
+    .url("./data/counties.json")
     .id("counties")
-    .on("load", mapReady));
-
-  // While map loads, get JSON
-  // Get JSON
-  $.getJSON("./data/future_sw.json", function(data) {
-    FUTURE_SW = data;
-    console.log(data);
+    .on("load", function(e) {
+      preparePaths(e); // Onload, prepare paths
+    }));
+  // While map loads, get geoJSON data
+  $.getJSON("./data/all_sw.json", function(data) {
+    ALL_SW = data;
   });
-  $.getJSON("./data/present_sw.json", function(data) {
-    PRESENT_SW = data;
-  });
-
-  function mapReady(e) {
-    preparePaths(e.features);
-  }
 
   var arePathsPrepared = false;
   var count = 0;
-  function preparePaths(features) {
+  function preparePaths(e) {
+    var features = e.features;
     if (arePathsPrepared) { // Don't run if already run
       return;
     }
     // Only go if GeoJSON and map is loaded
-    if (FUTURE_SW === undefined || PRESENT_SW === undefined) {
+    if (ALL_SW === undefined) {
       if (count < 10) {
         count++;
         console.log("JSON not ready, trying again: try #"+ count);
         setTimeout(function() {
-          preparePaths(features);
+          preparePaths(e);
         }, 200);
       } else {
-        console.log("JSON not ready, tried 10 times. Stopping.");
         alert("The data failed to load—please check your connection and " +
           "refresh the page.")
       }
       return;
     }
+    console.log(ALL_SW);
     arePathsPrepared = true;
     // Attach data to each path
     $.each(features, function(i, f) {
       var p = f.data.properties;
       var fips = p["STATE"] + p["COUNTY"];   // Get fips code
-      if (PRESENT_SW[fips] === undefined || FUTURE_SW[fips] === undefined) {
-        $(f.element).attr("class", "disabled"); // If no data, disable
+      if (ALL_SW[fips] === undefined) {
+        // If no data, disable. This should, in theory, never happen because
+        // all counties with no data were removed from the counties.json file.
+        $(f.element).attr("class", "disabled");
       } else {
-        roundValues(fips);
+        // Store data on each <path> element.
         $(f.element).attr("fips", fips);     // Add fips code as "fips" attr
         $(f.element).attr("name", p["NAME"]);// Add county name as "name" attr
+        colorCounty(f.element);                // Color the county accordingly
       }
     });
     // Attach event handlers to available paths
     $("path:not(.disabled)").click(function(e) {
       var elem = $(e.currentTarget);
       populateData(elem.attr("fips"), elem.attr("name"));
-      $("path.selected").attr("class", "");   // Turn coloring off
-      elem.attr("class", "selected");         // Highlight clicked county
+      removeClass($("path.selected"), "selected"); // Remove previous highlight
+      addClass(elem, "selected");                  // Highlight clicked county 
     });
+    // Attach event handlers to checkboxes
+    $("input[name='smoke-map-time']").click(function(e) {
+      MODE = $(e.currentTarget).data("time");
+      // Recolor map
+      console.log(MODE);
+      $.each($("#counties path"), function(i, p) {
+        colorCounty(p);
+      });
+    });
+  }
+
+  /* 
+   * Given a county feature, assigns correct class depending on 
+   * given mode. The element should have a 'fips' attribute with its code.
+   */
+  function colorCounty(element) {
+    var fips = $(element).attr("fips");
+    var classes = ["index-0", "index-1", "index-2", "index-3", "index-4",
+      "index-5"];
+    var key = MODE === "future" ? KEYS.future.index : KEYS.present.index;
+    console.log(key);
+    // Get the corresponding class from the 'classes' array.
+    var c = classes[ALL_SW[fips][key]];
+    $.each(classes, function(i, c) {
+      // Remove any previous classes
+      removeClass(element, c);
+    });
+    // Add correct class
+    addClass(element, c);
   }
 
   /*
    * Given a fips code, populates the data-container div with the corresponding
    * HTML
    */
-  function populateData(fips, name) {
-    var data = {}
-    data.name = name;
-    data.present = PRESENT_SW[fips];
-    data.future = FUTURE_SW[fips];
-    if (!data.present || !data.future) {
+  function populateData(fips) {
+    var data = ALL_SW[fips];
+    if (!data) {
       return;
     }
     var html = $.templates("#dataContainerTmpl").render(data);
-    $("#data-container").html(html);
-    $("#data-container").addClass("shown"); // Show data container
+    $(DATA_CONTAINER_ID).html(html).addClass("shown");
   }
 
-  function roundValues(fips) {
-    var p = PRESENT_SW[fips];
-    $.each(p, function(key, value) {
-      if (!isNaN(parseInt(value))) {
-        p[key] = Math.round(p[key] * 100) / 100;
-      }
-    });
-    var f = FUTURE_SW[fips];
-    $.each(f, function(key, value) {
-      if (!isNaN(parseInt(value))) {
-        f[key] = Math.round(f[key] * 100) / 100;
-      }
-    });
+  /* jQuery add/removeClass doesn't work on SVG, so writing my own... */
+  function addClass(elem, newClass) {
+    if (elem.length !== undefined) { // is an array
+      $.each(elem, function(i, e) {
+        addClass_(e, newClass);
+      });
+    } else {
+      addClass_(elem, newClass);
+    }
+  }
+  function addClass_(elem, newClass) {
+    $(elem).attr("class", $(elem).attr("class") + " " + newClass);
+  }
+  function removeClass(elem, rmClass) {
+    if (elem.length !== undefined) { // is an array
+      $.each(elem, function(i, e) {
+        removeClass_(e, rmClass);
+      });
+    } else {
+      removeClass_(elem, rmClass);
+    }
+  }
+  function removeClass_(elem, rmClass) {
+    var cs = $(elem).attr("class");
+    if (!cs) {
+      return;
+    }
+    cs = cs.split(" ");
+    var i;
+    while (i = cs.indexOf(rmClass) !== -1) {
+      cs.splice(i, 1);
+    }
+    $(elem).attr("class", cs.join(" "));
   }
 });
+
+
+  // function roundValues(fips) {
+  //   var a = ALL_SW[fips];
+  //   $.each(a, function(key, value) {
+  //     if (!isNaN(parseInt(value))) {
+  //       a[key] = Math.round(a[key] * 100) / 100;
+  //     }
+  //   });
+  // }
